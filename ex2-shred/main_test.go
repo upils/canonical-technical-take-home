9fe590127b96562bd9df333d8db85642c6200d9e
paul <upils@eodras.com>
2023-04-03 23:11:15 +0200

    Add basic tests

---
+++ ex2-shred/main_test.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"errors"
+	"os"
+	"testing"
+)
+
+func Test_shred(t *testing.T) {
+	type args struct {
+		path       string
+		iterations uint
+	}
+	tests := []struct {
+		name         string
+		args         args
+		createTarget bool
+		wantErr      error
+	}{
+		{
+			name: "shred successfully",
+			args: args{
+				path:       "tests/test.txt",
+				iterations: 5,
+			},
+			createTarget: true,
+			wantErr:      nil,
+		},
+
+		{
+			name: "empty path",
+			args: args{
+				path:       "",
+				iterations: 5,
+			},
+			wantErr: ErrEmptyPath,
+		},
+		{
+			name: "0 iterations",
+			args: args{
+				path:       "tests/test.txt",
+				iterations: 0,
+			},
+			wantErr: ErrNoIterration,
+		},
+		{
+			name: "impossible to shred a directory",
+			args: args{
+				path:       "tests/",
+				iterations: 5,
+			},
+			wantErr: ErrPathIsDir,
+		},
+		{
+			name: "inexistent file",
+			args: args{
+				path:       "tests/non-existent.txt",
+				iterations: 5,
+			},
+			wantErr: os.ErrNotExist,
+		},
+	}
+	for _, tt := range tests {
+		if tt.createTarget {
+			err := os.WriteFile(tt.args.path, []byte("test"), 0655)
+			if err != nil {
+				t.Fatal(err)
+			}
+		}
+
+		t.Run(tt.name, func(t *testing.T) {
+			err := shred(tt.args.path, tt.args.iterations)
+			if (err != nil) && !errors.Is(err, tt.wantErr) {
+				t.Errorf("shred() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
