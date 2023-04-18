8efc17b8267e7f6fb74218f172fceddbc245f8d3
paul <upils@eodras.com>
2023-04-18 08:55:00 +0200

    Help understand various steps

--- ex1-bootable-linux/hello.sh
+++ ex1-bootable-linux/hello.sh
@@ -5,10 +5,13 @@ set -o pipefail
 
 TOP=$PWD
 
+echo "Download kernel source code"
 curl https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.2.10.tar.xz | tar xJf -
+echo "Download busybox source code"
 curl https://busybox.net/downloads/busybox-1.36.0.tar.bz2 | tar xjf - 
 
-sudo apt-get update && apt-get install -y \
+echo "Install needed tools"
+sudo apt-get update && sudo apt-get install -y \
         bc \
         bison \
         build-essential \
@@ -20,30 +23,33 @@ sudo apt-get update && apt-get install -y \
         qemu-utils \
         qemu-system-x86
 
-cd busybox-1.36.0 && \
-  mkdir -pv $TOP/obj/busybox-x86 && \
-  make O=$TOP/obj/busybox-x86 defconfig && \
+echo "Configure busybox build"
+cd busybox-1.36.0
+mkdir -pv $TOP/obj/busybox-x86
+CONFIG_EXTRA_CFLAGS="-static" make O=$TOP/obj/busybox-x86 defconfig
 
-echo -n "CONFIG_STATIC=y" > $TOP/obj/busybox-x86/.config
+echo "Build busybox"
+cd $TOP/obj/busybox-x86
+make -j$(nproc)
+make install
 
-cd $TOP/obj/busybox-x86 && \
-  make -j$(nproc) && \
-  make install
+echo "Prepare initramfs"
+cd $TOP/initramfs/x86-busybox
+mkdir -pv {bin,sbin,etc,proc,sys,usr/{bin,sbin}}
+cp -av $TOP/obj/busybox-x86/_install/* .
+cd $TOP/initramfs/x86-busybox
+find . -print0 \
+  | cpio --null -ov --format=newc \
+  | gzip -9 > $TOP/obj/initramfs-busybox-x86.cpio.gz
 
-cd $TOP/initramfs/x86-busybox && \
-  mkdir -pv {bin,sbin,etc,proc,sys,usr/{bin,sbin}} && \
-  cp -av $TOP/obj/busybox-x86/_install/* .
+echo "Configure kernel"
+cd $TOP/linux-6.2.10
+make O=$TOP/obj/linux-x86-basic x86_64_defconfig
+make O=$TOP/obj/linux-x86-basic kvm_guest.config
+echo "Build kernel"
+make O=$TOP/obj/linux-x86-basic -j$(nproc)
 
-cd $TOP/initramfs/x86-busybox && \
-  find . -print0 \
-    | cpio --null -ov --format=newc \
-    | gzip -9 > $TOP/obj/initramfs-busybox-x86.cpio.gz
-
-cd $TOP/linux-6.2.10 && \
-  make O=$TOP/obj/linux-x86-basic x86_64_defconfig && \
-  make O=$TOP/obj/linux-x86-basic kvm_guest.config && \
-  make O=$TOP/obj/linux-x86-basic -j$(nproc)
-
+echo "Boot"
 cd $TOP
 sudo qemu-system-x86_64 \
     -kernel obj/linux-x86-basic/arch/x86_64/boot/bzImage \
